e3a0fc78526f3e5e1656a8c801d0a044cf302eb7
Gonzalo <gonzalomerida@hotmail.com>
2023-11-10 12:26:25 +0100

    Codificar

--- app/src/main/java/com/example/gmergames/CreditActivity.kt
+++ app/src/main/java/com/example/gmergames/CreditActivity.kt
@@ -1,4 +1,11 @@
 package com.example.gmergames
 
-class CreditActivity {
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class CreditActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_credit)
+    }
 }
