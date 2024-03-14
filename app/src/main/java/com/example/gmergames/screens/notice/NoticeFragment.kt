e78d069a66d7efebd08d65e882c2694467a33d54
Gonzalo <gonzalomerida@hotmail.com>
2024-03-14 14:05:23 +0100

    Revisar los Notice, y la API

--- app/src/main/java/com/example/gmergames/screens/notice/NoticeFragment.kt
+++ app/src/main/java/com/example/gmergames/screens/notice/NoticeFragment.kt
@@ -9,12 +9,13 @@ import androidx.fragment.app.viewModels
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import com.google.android.material.tabs.TabLayoutMediator
 import com.example.gmergames.databinding.FragmentNoticeBinding
+import com.google.android.material.tabs.TabLayout
 
 //FRAMGENTO QUE CONTIENE EL VIEWPAGER.
 //EN LA PARTE INFERIOR ESTÁ DECLARADA LA CLASE ADAPTADOR DEL VIEWPAGER
 class NoticeFragment : Fragment() {
 
-    private var _binding : FragmentNoticeBinding? = null
+    private var _binding: FragmentNoticeBinding? = null
     private val binding
         get() = _binding!!
 
@@ -32,7 +33,7 @@ class NoticeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        _binding = FragmentNoticeBinding.inflate(inflater,container,false)
+        _binding = FragmentNoticeBinding.inflate(inflater, container, false)
         return binding.root
     }
 
@@ -41,11 +42,11 @@ class NoticeFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         binding.vpNotice.adapter = NoticeAdapter(this)
-	
-	//ACTIVAR LAS PESTAÑAS CON SCROLL
-	//binding.tabNotice.tabMode = TabLayout.MODE_SCROLLABLE
 
-	//GENERA UNA PESTAÑA POR CADA ELEMENTO DEL VIEWPAGER. EN ESTE CASO: 2.
+        //ACTIVAR LAS PESTAÑAS CON SCROLL
+        binding.tabNotice.tabMode = TabLayout.MODE_SCROLLABLE
+
+        //GENERA UNA PESTAÑA POR CADA ELEMENTO DEL VIEWPAGER. EN ESTE CASO: 2.
         TabLayoutMediator(binding.tabNotice, binding.vpNotice) { tab, position ->
             tab.text = when (position) {
                 0 -> "Primero"
@@ -58,15 +59,15 @@ class NoticeFragment : Fragment() {
 //ADAPTADOR PARA EL VIEWPAGER.
 class NoticeAdapter(fragment: Fragment) : FragmentStateAdapter(fragment) {
 
-	//NUMERO DE ELEMENTOS O FRAGMENTOS A CARGAR. EN ESTE CASO:2.
-	//PODRÍA TOMAR LOS VALORES DEL TAMAÑO DE UNA LISTA, POR EJEMPLO.
+    //NUMERO DE ELEMENTOS O FRAGMENTOS A CARGAR. EN ESTE CASO:2.
+    //PODRÍA TOMAR LOS VALORES DEL TAMAÑO DE UNA LISTA, POR EJEMPLO.
     override fun getItemCount(): Int = 2
 
-	//FUNCIÓN PARA CREAR LOS FRAGMENTOS (ASOCIAR). SEGÚN LA POSICIÓN RETORNARÁ UN FRAGMENTO U OTRO.
-	//SE PODRÍA AUTOMATIZAR Y QUE SE LLAME A UN MISMO FRAGMENTO AL QUE LE LLEGAN DATOS QUE VARÍAN.
+    //FUNCIÓN PARA CREAR LOS FRAGMENTOS (ASOCIAR). SEGÚN LA POSICIÓN RETORNARÁ UN FRAGMENTO U OTRO.
+    //SE PODRÍA AUTOMATIZAR Y QUE SE LLAME A UN MISMO FRAGMENTO AL QUE LE LLEGAN DATOS QUE VARÍAN.
     override fun createFragment(position: Int): Fragment {
         // Return a NEW fragment instance in createFragment(int)
-        val fragment = if(position==0)
+        val fragment = if (position == 0)
             Tab1NoticeFragment()
         else
             Tab2NoticeFragment()
